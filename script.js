4dac0b920aaf0593521fe056821f8789c49dc0e9
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-14 18:53:49 -0500

    script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+function generateText() {
+  const promptInput = document.getElementById('prompt').value;
+  fetch('/.netlify/functions/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: promptInput }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('API Response:', data); // Log the API response
+      document.getElementById('result').textContent = data.generated_text;
+    })
+    .catch((error) => {
+      console.error('Error:', error); // Log any errors
+    });
+}
+
 document.getElementById('form').addEventListener('submit', function(e) {
     e.preventDefault();
-
-    var prompt = document.getElementById('prompt').value;
-
-    fetch('/.netlify/functions/generate', {
-        method: 'POST',
-        body: JSON.stringify({ prompt: prompt })
-    })
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('result').innerText = data.generated_text;
-    });
+    generateText();
 });
