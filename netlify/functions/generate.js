78bd6f2e6f71a05be6958a1919d2e8d942c4469b
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-15 00:20:31 -0500

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -1,12 +1,12 @@
-const openai = require('openai');
+const { OpenAI } = require('openai');
 
-openai.apiKey = process.env.OPENAI_API_KEY;
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
 exports.handler = async function(event, context) {
     const data = JSON.parse(event.body);
     const prompt = data.prompt;
 
-    const response = await openai.Completion.create({
+    const response = await openai.complete({
         engine: 'text-davinci-003',
         prompt: prompt,
         temperature: 0.9,
@@ -16,7 +16,7 @@ exports.handler = async function(event, context) {
     return {
         statusCode: 200,
         body: JSON.stringify({
-            generated_text: response.choices[0].text.strip()
+            generated_text: response.data.choices[0].text
         })
     };
 };
