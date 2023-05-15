6b7de81be8565fddd17ad7d9927c42539d2b440a
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-14 20:46:18 -0500

    Initial commit

---
+++ netlify/functions/generate.js
@@ -0,0 +1,24 @@
+const openai = require('openai');
+const axios = require('axios');
+
+
+const api = new openai.OpenAi(process.env.OPENAI_API_KEY);
+
+exports.handler = async function(event, context) {
+    const data = JSON.parse(event.body);
+    const prompt = data.prompt;
+
+    const response = await api.Completion.create({
+        engine: 'text-davinci-003',
+        prompt: prompt,
+        temperature: 0.9,
+        max_tokens: 100
+    });
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            generated_text: response.choices[0].text.strip()
+        })
+    };
+};
