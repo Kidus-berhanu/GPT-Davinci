7c911efbe6c55cc8743496283e17556381168601
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-15 17:05:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to generate text
 function generateText() {
   const promptInput = document.getElementById('prompt').value;
   fetch('/.netlify/functions/generate', {
@@ -11,13 +12,35 @@ function generateText() {
     .then((data) => {
       console.log('API Response:', data); // Log the API response
       document.getElementById('result').textContent = data.generated_text;
+      document.getElementById('word-count').textContent = "Word count: " + data.generated_text.split(' ').length;
     })
     .catch((error) => {
       console.error('Error:', error); // Log any errors
     });
 }
 
+// Event listener for the form submission
 document.getElementById('form').addEventListener('submit', function(e) {
     e.preventDefault();
     generateText();
+});
+
+// Function to switch theme
+document.getElementById('theme-switch').addEventListener('click', function() {
+    let root = document.documentElement;
+    let bgColor = getComputedStyle(root).getPropertyValue('--bg-color');
+    let textColor = getComputedStyle(root).getPropertyValue('--text-color');
+    root.style.setProperty('--bg-color', textColor);
+    root.style.setProperty('--text-color', bgColor);
+});
+
+// Function to change font size
+document.getElementById('font-size').addEventListener('click', function() {
+    let prompt = document.getElementById('prompt');
+    let result = document.getElementById('result');
+    let currentFontSize = parseFloat(window.getComputedStyle(prompt, null).getPropertyValue('font-size'));
+    let newFontSize = currentFontSize + 1;
+    if (newFontSize > 30) newFontSize = 16;
+    prompt.style.fontSize = newFontSize + 'px';
+    result.style.fontSize = newFontSize + 'px';
 });
