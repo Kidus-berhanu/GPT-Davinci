47d886840486ecbc9c06ff6e79c867ae158ef05c
Kidus Berhanu <107410165+Kidus-berhanu@users.noreply.github.com>
2023-05-15 21:17:43 -0500

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -15,7 +15,7 @@ exports.handler = async function(event, context) {
       model: 'text-davinci-003',
       prompt: prompt,
       temperature: 0.9,
-      max_tokens: 200
+      max_tokens: 100
     });
 
     return {
